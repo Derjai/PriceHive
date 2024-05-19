f87bfdefb7885ae9d58e2e91b168bba6f01c62ee
Derjai <ojaider@uninorte.edu.co>
2024-05-19 16:59:45 -0500

    Precios: - Implementado tomar primeros 5 productos y de estos seleccionar los tres con menor precio. - Ya se extraen los precios de olimpica, aún faltan el nombre, la imágen y los links.

--- prices.js
+++ prices.js
@@ -13,6 +13,38 @@ function toHTML(prices, titles, imgs, links) {
   return html;
 }
 
+async function getOlimpicaValues(values, titles, links, imgs) {
+  var valuesText = await Promise.all(
+    values.map(async (element) => {
+      const priceElements = await element.$$('.olimpica-dinamic-flags-0-x-currencyInteger');
+      const priceParts = await Promise.all(priceElements.map(priceElement => priceElement.evaluate(el => el.innerText)));
+      const price = priceParts.join('');
+      return price;
+    })
+  );
+
+  var titlesText = await Promise.all(
+    titles.map(async (element) => {
+      return await element.evaluate((node) => node.innerText.trim());
+    })
+  );
+
+  var imgsSrc = await Promise.all(
+    imgs.map(async (element) => {
+      return await element.evaluate((node) => node.getAttribute("src"));
+    })
+  );
+
+  // Get the text content of each element in the titles array
+  var linksSrc = await Promise.all(
+    links.map(async (element) => {
+      return await element.evaluate((node) => node.getAttribute("href"));
+    })
+  );
+
+  return [valuesText, titlesText, linksSrc, imgsSrc];
+}
+
 async function getValues(values, titles, links, imgs) {
   var valuesText = await Promise.all(
     values.map(async (element) => {
@@ -43,6 +75,30 @@ async function getValues(values, titles, links, imgs) {
   return [valuesText, titlesText, linksSrc, imgsSrc];
 }
 
+async function getCheapestProducts(values, titles, links, imgs, addStoreLink=true,addStoreImg=true,Store) {
+  values = values.slice(0,5);
+  titles = titles.slice(0,5);
+  links = links.slice(0,5);
+  imgs = imgs.slice(0,5);
+
+  if (Store === "https://www.olimpica.com/"){
+    [valuesText,titlesText,linksSrc,imgsSrc]= await getOlimpicaValues(values,titles,links,imgs);
+    
+  } else {
+    [valuesText,titlesText,linksSrc,imgsSrc]= await getValues(values,titles,links,imgs);
+  }
+
+  const products= valuesText.map((price,i)=>({
+    title: titlesText[i],
+    price: parseFloat(price.replace(/\./g, '').replace(/[^0-9.-]+/g,"")),
+    link: addStoreLink ? Store + linksSrc[i]: linksSrc[i],
+    img: addStoreImg ? Store + imgsSrc[i]: imgsSrc[i]
+  }))
+  products.sort((a,b)=> a.price - b.price);
+  const cheapestThreeProducts = products.slice(0,3);
+  return cheapestThreeProducts;
+}
+
 async function mercadoLibrePrices() {
   const browser = await chromium.launch({ headless: true });
   const page = await browser.newPage();
@@ -70,19 +126,14 @@ async function mercadoLibrePrices() {
     ".ui-search-item__group__element.ui-search-link__title-card.ui-search-link"
   );
 
-  [valuesText, titlesText, linksSrc, imgsSrc] = await getValues(
-    values,
-    titles,
-    links,
-    imgs
-  );
+  const cheapestProducts = await getCheapestProducts(values,titles,links,imgs,false,false,"https://www.mercadolibre.com.co/");
 
   await browser.close();
   return toHTML(
-    valuesText.slice(0, 3),
-    titlesText.slice(0, 3),
-    imgsSrc.slice(0, 3),
-    linksSrc.slice(0, 3)
+    cheapestProducts.map((product) => product.price),
+    cheapestProducts.map((product) => product.title),
+    cheapestProducts.map((product) => product.img),
+    cheapestProducts.map((product) => product.link)
   );
 }
 
@@ -94,46 +145,22 @@ async function olimpicaPrices() {
   const searchBar = await page.locator('input[id^="downshift-"][id$="-input"]');
   await searchBar.fill("Samsung S20");
   await page.keyboard.press("Enter");
-  await page.waitForLoadState("domcontentloaded");
+  await page.waitForSelector('.vtex-search-result-3-x-galleryItem', { state: 'attached' });
+  const products = await page.$$('.vtex-search-result-3-x-galleryItem', { state: 'attached' });
+  console.log(`Found ${products.length} products`); 
+  var values = await page.$$('.vtex-product-price-1-x-sellingPrice--hasListPrice--dynamicF');
+  var titles = await page.$$('vtex-product-summary-2-x-productBrand vtex-product-summary-2-x-brandName t-body');
+  var links = await page.$$('vtex-product-summary-2-x-clearLink vtex-product-summary-2-x-clearLink--product-summary h-100 flex flex-column');
+  var imgs = await page.$$('vtex-product-summary-2-x-imageNormal vtex-product-summary-2-x-image vtex-product-summary-2-x-mainImageHovered');
 
-  await page.waitForSelector(
-    ".vtex-search-result-3-x-galleryItem.vtex-search-result-3-x-galleryItem--normal.vtex-search-result-3-x-galleryItem--grid-3"
-  );
-
-  var actualValueContainers = await page.$$(
-    ".vtex-product-price-1-x-sellingPrice--hasListPrice--dynamicF"
-  );
-
-  var values = await actualValueContainers.map(async (container) => {
-    return await container.$$(".olimpica-dinamic-flags-0-x-currencyInteger");
-  });
-
-  var titles = await page.$$(
-    ".vtex-product-summary-2-x-productBrand vtex-product-summary-2-x-brandName t-body"
-  );
-  var imgs = await page.$$(
-    ".vtex-product-summary-2-x-imageNormal vtex-product-summary-2-x-image vtex-product-summary-2-x-mainImageHovered"
-  );
-  var links = await page.$$(
-    ".vtex-product-summary-2-x-clearLink vtex-product-summary-2-x-clearLink--product-summary h-100 flex flex-column"
-  );
-
-  console.log(actualValueContainers);
-  //console.log(titles)
-  //console.log(imgs)
-  //console.log(links)
-
-  //[valuesText,titlesText,linksSrc,imgsSrc] = await getValues(values,titles,imgs,links)
-
-  //console.log(valuesText)
-
+  const cheapestProducts = await getCheapestProducts(values,titles,links,imgs,false,false,"https://www.olimpica.com/");
   await browser.close();
-  // return toHTML(
-  //   groupedValues.slice(0, 2),
-  //   titlesText.slice(0, 2),
-  //   imgsSrc.slice(0, 2),
-  //   linksSrc.slice(0, 2)
-  // );
+  return toHTML(
+    cheapestProducts.map((product) => product.price),
+    cheapestProducts.map((product) => product.title),
+    cheapestProducts.map((product) => product.img),
+    cheapestProducts.map((product) => product.link)
+  );
 }
 
 async function alkostoPrices() {
@@ -158,27 +185,14 @@ async function alkostoPrices() {
     ".product__item__information__image.js-algolia-product-click img"
   );
 
-  [valuesText, titlesText, linksSrc, imgsSrc] = await getValues(
-    values,
-    titles,
-    links,
-    imgs
-  );
+  const cheapestProducts = await getCheapestProducts(values,titles,links,imgs,true,true,"https://www.alkosto.com/");
 
-  linksSrc = linksSrc.map((src) => {
-    return "https://www.alkosto.com" + src;
-  });
-  imgsSrc = imgsSrc.map((src) => {
-    return "https://www.alkosto.com" + src;
-  });
-
-  console.log(imgsSrc);
   await browser.close();
   return toHTML(
-    valuesText.slice(0, 2),
-    titlesText.slice(0, 2),
-    imgsSrc.slice(0, 2),
-    linksSrc.slice(0, 2)
+    cheapestProducts.map((product)=>product.price),
+    cheapestProducts.map((product)=>product.title),
+    cheapestProducts.map((product)=>product.img),
+    cheapestProducts.map((product)=>product.link)
   );
 }
 
@@ -198,38 +212,14 @@ async function exitoPrices() {
   var titleLinks = await page.$$('a[data-testid="product-link"][title]')
   var imgs = await page.$$(".imagen_plp");
 
-  var valuesText = await Promise.all(
-    values.map(async (element) => {
-      return await element.evaluate((node) => node.textContent.trim());
-    })
-  );
-
-  var titlesText = await Promise.all(
-    titleLinks.map(async (element) => {
-      return await element.evaluate((node) => node.getAttribute('title'));
-    })
-  );
-
-  var imgsSrc = await Promise.all(
-    imgs.map(async (element) => {
-      return await element.evaluate((node) => node.getAttribute("src"));
-    })
-  );
-
-  var linksSrc = await Promise.all(
-    titleLinks.map(async (element) => {
-      return await element.evaluate((node) => "https://www.exito.com"+node.getAttribute("href"));
-    })
-  );
-
-  console.log(titlesText)
+  const cheapestThreeProducts = await getCheapestProducts(values,titleLinks,titleLinks,imgs,true,false,"https://www.exito.com/");
 
   await browser.close();
   return toHTML(
-    valuesText.slice(0, 3),
-    titlesText.slice(0, 3),
-    imgsSrc.slice(0, 3),
-    linksSrc.slice(0, 3)
+    cheapestThreeProducts.map((product)=>product.price),
+    cheapestThreeProducts.map((product)=>product.title),
+    cheapestThreeProducts.map((product)=>product.img),
+    cheapestThreeProducts.map((product)=>product.link)
   );
 }
 
@@ -260,20 +250,14 @@ async function falabellaPrices() {
     }
   });
 
-
-  [valuesText, titlesText, linksSrc, imgsSrc] = await getValues(
-    values,
-    titles,
-    links,
-    imgs
-  );
+  const cheapestProducts = await getCheapestProducts(values,titles,links,imgs,false,false,"https://www.falabella.com.co/falabella-co/");
 
   await browser.close();
   return toHTML(
-    valuesText.slice(0, 3),
-    titlesText.slice(0, 3),
-    imgsSrc.slice(0, 3),
-    linksSrc.slice(0, 3)
+    cheapestProducts.map((product) => product.price),
+    cheapestProducts.map((product) => product.title),
+    cheapestProducts.map((product) => product.img),
+    cheapestProducts.map((product) => product.link)
   );
 }
 
